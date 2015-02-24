11156fe150d9a3173002f298d4b2965fdfd24e64
Andreas Dahl <andreas@hrdahl.dk>
2015-02-24 16:11:37 +0100

    Benchmarking shell script

---
+++ bench.sh
@@ -0,0 +1,14 @@
+rm -f bench.tmp
+
+for x in {1..10}
+do
+    gtime -f "%e" -o bench.txt -a $@
+done
+
+awk '
+BEGIN {max = 0; min = 100000}
+    {s+=$0}
+    {n+=1}
+    {if (max<$0) max=$0}
+    {if (min>$0) min=$0}
+END {print "avg: " s/n; print "min: " min; print "max: " max}' bench.txt
