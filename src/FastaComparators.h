b2e23435f9433b2bd2a47e2c6d25e837c8a8a9e4
Andreas Dahl <andreas@hrdahl.dk>
2015-03-23 23:32:31 +0100

    Fixed bug in Kmer comparator

--- src/FastaComparators.h
+++ src/FastaComparators.h
@@ -9,7 +9,7 @@
 
 struct LengthThenKmer
 {
-    static inline bool less_than (const FastaContainer& fa1, const FastaContainer& fa2)
+    static inline bool less_than(const FastaContainer& fa1, const FastaContainer& fa2)
     {
         unsigned int permutations = 1 << (fa1.k << 1); // 4^k == 2^(k*2)
         if (fa1.sequence.size() < fa2.sequence.size()) {
@@ -24,27 +24,25 @@ struct LengthThenKmer
         return false;
     }
 
-    static inline bool greater_than (const FastaContainer& fa1, const FastaContainer& fa2) {
+    static inline bool greater_than(const FastaContainer& fa1, const FastaContainer& fa2) {
         return LengthThenKmer::less_than(fa2, fa1);
     }
 };
 
 struct Kmer
 {
-    static inline bool less_than (const FastaContainer& fa1, const FastaContainer& fa2)
+    static inline bool less_than(const FastaContainer& fa1, const FastaContainer& fa2)
     {
         unsigned int permutations = 1 << (fa1.k << 1); // 4^k == 2^(k*2)
-        if (fa1.sequence.size() == fa2.sequence.size()) {
-            for (unsigned int i = 0; i < permutations; ++i) {
-                if (fa1.kMer[i] < fa2.kMer[i]) {
-                    return true;
-                }
+        for (unsigned int i = 0; i < permutations; ++i) {
+            if (fa1.kMer[i] < fa2.kMer[i]) {
+                return true;
             }
         }
         return false;
     }
 
-    static inline bool greater_than (const FastaContainer& fa1, const FastaContainer& fa2) {
+    static inline bool greater_than(const FastaContainer& fa1, const FastaContainer& fa2) {
         return Kmer::less_than(fa2, fa1);
     }
 };
