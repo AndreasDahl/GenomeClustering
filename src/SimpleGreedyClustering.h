81555296eca9c83cf5a05ccfcad0b2769ce8c98b
Andreas Dahl <andreas@hrdahl.dk>
2015-03-23 13:54:28 +0100

    SimpleGreedyClustering now uses a LRU cache for centroids
    Cluster count from 7988 to 6846 on small data with 16824 entries
    
    fixes #2

--- src/SimpleGreedyClustering.h
+++ src/SimpleGreedyClustering.h
@@ -15,7 +15,7 @@
 
 template <typename T>
 void simpleGreedyClustering(std::vector<T>& data, float (*dist)(T  &, T  &), float similarity) {
-    std::list<T> centroids;
+    std::list<T*> centroids;
     std::cout << data.size() << std::endl;
     int c_count = 0;
     int n = 0;
@@ -26,10 +26,12 @@ void simpleGreedyClustering(std::vector<T>& data, float (*dist)(T  &, T  &), flo
             std::cout.flush();
         }
         float bestDist = std::numeric_limits<float>::infinity();
-        for (typename std::list<T>::iterator it = centroids.begin(); it != centroids.end(); ++it) {
-            float distance = dist(*current, *it);
+        T* bestCent;
+        for (typename std::list<T*>::iterator it = centroids.begin(); it != centroids.end(); ++it) {
+            float distance = dist(*current, **it);
             if (distance < similarity) {
                 bestDist = distance;
+                bestCent = *it;
                 break;
             }
         }
@@ -38,35 +40,13 @@ void simpleGreedyClustering(std::vector<T>& data, float (*dist)(T  &, T  &), flo
             if (centroids.size() >= 32) {
                 centroids.pop_back();
             }
-            centroids.push_front(*current);
+            centroids.push_front(&(*current));
             c_count++;
-//            std::cout
-//                    << "S\t"
-//                    << centroids.size() - 1 << "\t"
-//                    << "SIZE\t"
-//                    << "*\t"
-//                    << "*\t"
-//                    << "*\t"
-//                    << "*\t"
-//                    << "*\t"
-//                    << "Name_" << n << "\t"
-//                    << "*";
         } else {
-//            std::cout
-//                    << "H\t"
-//                    << bestIndex << "\t"
-//                    << "SIZE\t"
-//                    << bestDist << "\t" // TODO: Percentage value
-//                    << "+\t"            // TODO: Determine Strand
-//                    << "*\t"            // TODO: 0-based coordinate of alignment start in the query sequence.
-//                    << "*\t"            // TODO: 0-based coordinate of alignment start in target (seed) sequence.
-//                                        //       If minus strand, Tlo is relative to start of reverse-complemented target.
-//                    << "*\t"            // TODO: Alignment?
-//                    << "Name_" << n << "\t"
-//                    << "Name_" << bestIndex;
+            // Move hit to front of cache
+            centroids.remove(bestCent);
+            centroids.push_front(bestCent);
         }
-//        std::cout << std::endl;
-//        std::cout.flush();
     }
     std::cout << "Cluster Count:" << c_count << std::endl << std::endl;
 }
