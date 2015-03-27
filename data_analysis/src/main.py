f9536a19da785a3618bc4368b5f0c4c7fbc93cda
Andreas Dahl <andreas@hrdahl.dk>
2015-03-27 16:50:09 +0100

    Sorting analysis

--- data_analysis/src/main.py
+++ data_analysis/src/main.py
@@ -24,8 +24,14 @@ def plot_hits(data, name='hits'):
     n = 10
     plt.figure()
     plt.title(name)
+    print len(data)
     plt.hist(data, 50, color='g')
     common = Counter(data).most_common(n)
+    median = np.median(data)
+    per95 = np.percentile(data, 95)
+    print "%s 95 percentile %.2f" % (name, per95)
+    plt.plot([median, median], plt.ylim(), c='b')
+    plt.plot([per95, per95], plt.ylim(), c='r')
     print name, "most common clusters:", common
     print "%.2f%% of values hit the same %d cluster" % (float(sum([v for _, v in common])) / len(data) * 100, n)
 
@@ -44,6 +50,8 @@ def plot_lookups(data, name='lookups'):
 if __name__ == "__main__":
     # load data
     full = load_data("res/length_muf_full.csv")
+    lkmer = load_data("res/dkmer_muf_full.csv")
+    kmer = load_data("res/kmer_muf_full.csv")
     greed = load_data("res/length_muf_greedy.csv")
     lru = load_data("res/length_muf_lru.csv")
 
@@ -53,6 +61,8 @@ if __name__ == "__main__":
     lb = [x for x in lru[:,1] if x != 0]
 
     plot_hits(full[:,0], "Best Hits")
+    plot_hits(kmer[:,0], "Kmer Hits")
+    plot_hits(lkmer[:,0], "Length-Kmer Hits")
     plot_hits(greed[:,0], "Greedy Hits")
 
     plot_lookups(nb, "Full lookups")
