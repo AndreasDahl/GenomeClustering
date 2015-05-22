c3965e44f728a4609cdc14755a38b6c853c553bd
Andreas Dahl <andreas@hrdahl.dk>
2015-05-22 20:01:15 +0200

    Output writing

---
+++ src/RecordWriter.cpp
@@ -0,0 +1,35 @@
+/*
+ * @Author: Andreas Dahl
+ * @Date: 22/05/15
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include "RecordWriter.h"
+
+const char DELIMITER = '\t';
+
+std::ostream& operator<<(std::ostream& out, const Record& record) {
+    switch(record.type) {
+        case HIT : out << "H"; break;
+        case CENTROID : out << "C"; break;
+    }
+    out << DELIMITER;
+    out << record.clusterNumber;
+    out << DELIMITER;
+    out << record.sequenceLength;
+    out << DELIMITER;
+    out.precision(2);
+    record.type == HIT 
+        ? out << record.id 
+        : out << '*';
+    out << DELIMITER;
+    out << record.query;
+    out << DELIMITER;
+    out << record.target;
+
+    return out;
+}
+
+
