b9a0125d2c734b2e82a304cabb3314f688a03f4a
Andreas Dahl <andreas@hrdahl.dk>
2015-06-08 02:12:25 +0200

    Added GPL v3 License

--- src/GreedyClustering.cpp
+++ src/GreedyClustering.cpp
@@ -1,4 +1,23 @@
 /*
+ * Copyright 2015 Andreas Dahl, Christian Muf
+ *
+ * This file is part of MufDahlClust.
+ *
+ * MufDahlClust is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MufDahlClust is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MufDahlCLust.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
  * @Author: Andreas Dahl 
  * @Author: Christian Muf
  * @Date: 27/03/15
